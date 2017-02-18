c9952e623b33474117bb835497de5b2706da51f7
kamitsoftware <googaal1990@gmail.com>
2017-02-18 21:21:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ app.get('/:articleName',function(req,res){
 
 app.get('/article-three',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+});*/
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});*/
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
