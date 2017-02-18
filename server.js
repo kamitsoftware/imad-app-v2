959b6d01936c9c2b4a626a2103ae5b645d7de9ba
kamitsoftware <googaal1990@gmail.com>
2017-02-18 21:17:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ return htmlTemplate;
 }
 
 
-app.get(':articleName',function(req,res){
+app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
