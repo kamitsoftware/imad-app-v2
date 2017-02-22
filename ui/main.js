11729790718defcf07d21f5bd116b7009d185014
kamitsoftware <googaal1990@gmail.com>
2017-02-22 20:08:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='new value';*/
 
 // move the image 
  
- var img = document.getElementById('madi');
+/* var img = document.getElementById('madi');
  var marginLeft=0;
  function moveRight()
  {
@@ -17,4 +17,12 @@ element.innerHTML='new value';*/
  }
  img.onclick = function() {
    var interval= setInterval(moveRight,100);  
+ };*/
+ 
+ var counter=0;
+ var button= document.getElementById('counter');
+ button.onclick=function(){
+     counter=counter+1;
+     var span=getElementById('count');
+     span.innerHTML=counter.toString();
  };
