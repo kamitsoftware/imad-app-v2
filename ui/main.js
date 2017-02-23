d7e26dd5aefd77393a8b05ffe1ed5f251f095068
kamitsoftware <googaal1990@gmail.com>
2017-02-23 23:37:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
 
 // change the text of the main text div
-var element= document.getElementById('main-text');
+/*var element= document.getElementById('main-text');
 
-element.innerHTML='new value';
+element.innerHTML='new value';*/
 
 // move the image 
  
@@ -19,7 +19,7 @@ element.innerHTML='new value';
    var interval= setInterval(moveRight,100);  
  };*/
  
- var counter=0;
+ //var counter=0;
  var button= document.getElementById('counter');
  button.onclick=function(){
      //make a requst to the counter endpoint
@@ -28,7 +28,7 @@ element.innerHTML='new value';
      //capture the response and store in variable
      request.onreadystatechange = function()
      {
-         if ( request.readyState === XMLHttpRequest.DONE)
+         if ( request.readyState === XMLHttpRequest.DONE){
          
          //take some action
          if(request.status===200)
@@ -37,6 +37,7 @@ element.innerHTML='new value';
                //counter=counter+1;
      var span=document.getElementById('count');
      span.innerHTML=counter.toString();
+         }
          }
      };
    request.open('GET','http://kamitsoftware.imad.hasura-app.io/counter',true);
