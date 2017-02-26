58f9d880256aefde7fea9118fdf65c6e65981514
kamitsoftware <googaal1990@gmail.com>
2017-02-26 20:10:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/:counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+    
+});
 
 var names=[];
 app.get('/submit-name/:name' , function(req,res){//url:/submit-name?name=aaa
@@ -107,12 +113,7 @@ app.get('/:articleName',function(req,res){
 
 
 
-var counter=0;
-app.get('/:counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-    
-});
+
 
 app.get('/ui/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one'));
